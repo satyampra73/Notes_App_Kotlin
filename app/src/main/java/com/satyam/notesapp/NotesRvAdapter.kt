6a316800de14b1e24f0e634567f44574856bd7d0
Satyam Prajapati <satyam7355pra@gmail.com>
2023-08-07 13:48:41 +0530

    working on recycler view corrent length is 11:00

---
+++ app/src/main/java/com/satyam/notesapp/NotesRvAdapter.kt
@@ -0,0 +1,40 @@
+package com.satyam.notesapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import kotlinx.coroutines.newFixedThreadPoolContext
+
+class NotesRvAdapter(val context:Context) : RecyclerView.Adapter<NotesRvAdapter.NoteViewHolder>() {
+
+    val allNotes=ArrayList<Note>()
+    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txtNote:TextView=itemView.findViewById(R.id.txtNote)
+        val imgDelete:ImageView=itemView.findViewById(R.id.imgDelete)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val viewHolder = NoteViewHolder(LayoutInflater.from(context).inflate(R.layout.item_note,parent,false))
+        return viewHolder
+    }
+
+    override fun getItemCount(): Int {
+        return allNotes.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+
+        holder.imgDelete.setOnClickListener{
+
+        }
+    }
+}
+
+interface INotesRvAdapter{
+    fun onItemClicked(note:Note)
+}
